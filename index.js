086a9227ea20abfdd3d5d458a07fd0b23998dd7c
Logan Cooper <ldtcoop@gmail.com>
2017-12-12 12:40:13 -0800

    Add credentials to passport

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 // creates running express application in node
 const app = express();
+
+// tells passport which strategies to use
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret
+}));
 
 // .get tells the express app to listen for a GET request to the route '/'
 app.get('/', (req, res) => {
