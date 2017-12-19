25d4ac1954959e9d0bb292d970e2edaa810a0248
Logan Cooper <ldtcoop@gmail.com>
2017-12-18 17:27:33 -0800

    Factor out promises in favor of async/await

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.deserializeUser((id, done) => {
 });
 
 // tells passport which strategies to use
-// arguments: object with clientID, clientSecret args, and route; accessToken callback
+// arguments: object with clientID, clientSecret args,
+// and route; accessToken callback
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -28,24 +29,23 @@ passport.use(
     callbackURL: '/auth/google/callback',
     // okays requests from proxies -- i.e. Heroku's load balancer
     proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
+  }, async (accessToken, refreshToken, profile, done) => {
     // queries DB for a user with the given googleId
     // this is an async action
-    User.findOne({ googleId: profile.id }).then((existingUser) => {
-      if (existingUser) {
-        // already have record with ID
-        // done function tells passport that everything is done
-        done(null, existingUser);
-      } else {
-        // no user record -- create and save new user
-        // this is also async
-        new User({googleId: profile.id})
-        .save()
-        .then((user) => {
-          // done takes an error argument and a user object
-          done(null, user);
-        });
-      }
+    const existingUser = await User.findOne({
+      googleId: profile.id
     });
-  }
-));
+    if (existingUser) {
+      // already have record with ID
+      // done function tells passport that everything is done
+      done(null, existingUser);
+    } else {
+      // no user record -- create and save new user
+      // this is also async
+      const user = new User({
+        googleId: profile.id
+      }).save();
+      // done takes an error argument and a user object
+      done(null, user);
+    }
+  }));
