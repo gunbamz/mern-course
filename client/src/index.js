be39d127a57f2a7ec2b234ded708fe16035d8a18
Logan Cooper <ldtcoop@gmail.com>
2017-12-19 15:40:14 -0800

    Add in materialize css

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 
+// imports css file into project -- no variable needed
+import 'materialize-css/dist/materialize.min.css';
+
 import App from './components/App';
 
 const store = createStore(reducers, {}, applyMiddleware());
