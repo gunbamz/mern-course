9197484e487321f81c5f8de8984b28e23410a79e
Logan Cooper <ldtcoop@gmail.com>
2017-12-28 19:57:51 -0800

    Add payments component skeleton

---
+++ client/src/components/Payments.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class ClassName extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <div>
+
+    </div>
+    );
+  }
+}
+
+export default ClassName;
