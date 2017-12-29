7b9c3ed4a332cfbd08923b9adc11284939c47956
Logan Cooper <ldtcoop@gmail.com>
2017-12-28 20:06:13 -0800

    Add config to payments component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -8,9 +8,11 @@ class ClassName extends React.Component {
 
   render() {
     return (
-    <div>
-
-    </div>
+    <StripeCheckout
+      amount={500}
+      token={token => console.log(token)}
+      stripeKey={process.env.REACT_APP_STRIPE_KEY}
+    />
     );
   }
 }
